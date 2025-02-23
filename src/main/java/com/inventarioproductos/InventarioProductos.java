3a101796eb001127d9453c22b1dc63dcdb3c7bcb
ElidallanaCristancho <104215083+Elidallanacristanchocaceres@users.noreply.github.com>
2025-02-23 16:50:01 -0500

    Update InventarioProductos.java

--- src/main/java/com/inventarioproductos/InventarioProductos.java
+++ src/main/java/com/inventarioproductos/InventarioProductos.java
@@ -1,7 +1,38 @@
 package com.inventarioproductos;
 
-public class Main {
+import java.util.*;
+
+public class InventarioProductos {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Map<String, List<String>> inventario = new HashMap<>();
+
+        // 1.cantidad de categorías
+        System.out.print("Ingrese la cantidad de categorías: ");
+        int cantidadCategorias = scanner.nextInt();
+        scanner.nextLine(); 
+
+        // 2.productos en cada categoria
+        for (int i = 0; i < cantidadCategorias; i++) {
+            System.out.print("Ingrese el nombre de la categoría " + (i + 1) + ": ");
+            String categoria = scanner.nextLine();
+
+            System.out.print("Ingrese la cantidad de productos en " + categoria + ": ");
+            int cantidadProductos = scanner.nextInt();
+            scanner.nextLine(); 
+
+            List<String> productos = new ArrayList<>();
+            for (int j = 0; j < cantidadProductos; j++) {
+                System.out.print("Ingrese el nombre del producto " + (j + 1) + ": ");
+                productos.add(scanner.nextLine());
+            }
+
+            inventario.put(categoria, productos);
+        }
+
+      
+
+        scanner.close();
     }
 }
+
